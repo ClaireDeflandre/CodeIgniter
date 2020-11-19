835e5238c5b2bc32e12c979b68d09a1d852c5e01
Claire Deflandre <67142033+ClaireDeflandre@users.noreply.github.com>
2020-11-19 14:48:14 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<div class="jumbotron">
+  <div class="container">
+    <h1><?=$title?></h1>
+    <p>Bienvenue sur Pick & Read</p>
+  </div>
+</div>
+<div class="container">
+  <p>Vous recherchez une boite à livres pres de chez vous ?</p>
+  <p>Vous souhaitez créer une boite à livres ?</p>
+  <p></p>
+</div>
